f192e97ab26c9f6e7c9ca412e52baf19fba32de7
Sam Cardin <blucardin@gmail.com>
2025-01-25 13:46:16 -0500

    meaningless commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+# cchange
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html', person='John Doe')
