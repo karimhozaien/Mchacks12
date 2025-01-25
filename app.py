2b2ef392385217a30d09b316f3a8ee7fd0d37db1
Sam Cardin <blucardin@gmail.com>
2025-01-25 14:02:17 -0500

    Add admin split

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
-# cchange
+import admin
+
 @app.route('/')
-def hello_world():  # put application's code here
+def app_introduction():
+
     return render_template('index.html', person='John Doe')
+
+
+@app.route('/heatmap')
+def heatmap():
+    return render_template('heatmap.html')
 
 if __name__ == '__main__':
     app.run()
